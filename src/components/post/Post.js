4acf1c90f7420e520be5d55c786db248cc01f74d
Oleksii-shy <alex27rider@gmail.com>
2021-06-22 12:22:27 +0300

    Homework 1 React

---
+++ src/components/post/Post.js
@@ -0,0 +1,9 @@
+export default function Post({item: {userId, id, title, body}}) {
+    return (
+        <div>
+            <h4>{id}. User id - {userId}</h4>
+            <h5>{title}</h5>
+            <p>{body}</p>
+        </div>
+    );
+}
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,18 @@
+import {useEffect, useState} from "react";
+import Post from "../post/Post";
+
+export default function Posts() {
+    let [postsList, setPostsList] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+                .then(value => value.json())
+                .then(value => {
+                    setPostsList(value);
+                });
+    }, [])
+    return (
+        <div>
+            {postsList.map((value, index) => <Post key={index} item={value}/>)}
+        </div>
+    );
+}
